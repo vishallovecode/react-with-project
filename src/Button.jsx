061062bf9710725464613e92355c2668b4f613b6
vishallovecode <785vishal443@gmail.com>
2023-10-21 22:44:46 +0530

    basic validation

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,10 @@
 const Button = (props) => {
   return (
-    <button className={props.classes} onClick={props.clickHandler}>
+    <button
+      className={props.classes}
+      disabled={props.disabled}
+      onClick={props.clickHandler}
+    >
       {props.buttonText || props.children}
     </button>
   );
--- src/component/userInputComponent/index.jsx
+++ src/component/userInputComponent/index.jsx
@@ -4,6 +4,13 @@ import Input from "../Input/input";
 // display container
 const LoginForm = (props) => {
   const { password, userName, onChange, loginHandler } = props;
+
+  const isDisabled = () => {
+    if (userName.length < 6 || password.length < 6) {
+      return true;
+    }
+    return false;
+  };
   return (
     <div className="flex m-auto  items-center w-96 justify-center flex-col gap-8 border px-20 py-20">
       <Input
@@ -24,7 +31,8 @@ const LoginForm = (props) => {
       />
       <Button
         clickHandler={loginHandler}
-        classes="border  w-60 p-2 rounded bg-red-600 text-white"
+        disabled={isDisabled()} // if disabled =true than button will be disabled
+        classes="border  w-60 p-2 rounded bg-red-600 text-white disabled:bg-gray-300"
       >
         Continue
       </Button>
@@ -33,3 +41,8 @@ const LoginForm = (props) => {
 };
 
 export default LoginForm;
+
+// if one of the field is empty than we  do not want to enable button
+// we want to enable button only when there value in both field
+// minimum length of username is 6  => means user name should have length greater than or equal to 6
+// minimum length of password is 6  =>means password should have length greater than or equal to 6
